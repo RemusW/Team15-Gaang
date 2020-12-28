08cea1fb6b97671641532bd92b8ab08035fa9122
caleb park <calebparky@gmail.com>
2020-12-27 18:18:56 -0600

    l/r facing experiment

---
+++ Assets/Team 15/Team/CP/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start() {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            print("A key was pressed");
+            sr.flipX = false;
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            print("d key was pressed");
+            sr.flipX = true;
+        }
+    }
+}
